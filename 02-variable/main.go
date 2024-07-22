f1a174d946d500e5dbf38c02b5f2d64f3b0c0b91
B FAROOQ <bfarooq2022@gmail.com>
2024-07-22 21:20:52 +0530

    files added

--- 02-variable/main.go
+++ 02-variable/main.go
@@ -2,9 +2,40 @@ package main
 
 import "fmt"
 
+//  %T is a format specifier used to find the TYPE of variable
+
 func main(){
+	//String type
 	var username string = "Farooq"
 	 
 	fmt.Println(username)
 	fmt.Printf("Variable type is  %T \n", username)
+
+	//Bool type
+	var isLogging bool = false	 
+
+	fmt.Println(isLogging)
+	fmt.Printf("Variable type is  %T \n", isLogging)
+
+	//Integer type
+	var smallVal int = 300	 
+
+	fmt.Println(smallVal)
+	fmt.Printf("Variable type is  %T \n", smallVal)
+
+	//Float32 and Float64
+	var smallFloat float32 = 300.5416574	 
+
+	fmt.Println(smallFloat)
+	fmt.Printf("Variable type is  %T \n", smallFloat)
+
+	//Implicit type
+	var myName = "Farooq"
+	fmt.Println(myName)
+	fmt.Printf("Variable type is  %T \n", myName)
+
+	//No var type
+	age := 21
+	fmt.Println(age)
+	fmt.Printf("%T",age)
 }
